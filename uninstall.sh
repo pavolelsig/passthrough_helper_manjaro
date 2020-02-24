9b179444190365700ad2ccb576028fb29b572ce6
pavolelsig <52982339+pavolelsig@users.noreply.github.com>
2020-02-23 19:08:53 -0800

    Add files via upload
    Allows users to easily revert changes

---
+++ uninstall.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+
+
+rm /usr/bin/vfio-pci-override.sh
+
+rm /etc/initcpio/install/vfio
+
+rm /etc/initcpio/hooks/vfio
+
+rm /etc/modprobe.d/vfio.conf
+
+
+if [ -a Backup/grub ]
+	then
+		rm /etc/default/grub
+		cp Backup/grub /etc/default/
+fi
+
+if [ -a Backup/mkinitcpio.conf ]
+	then
+		rm /etc/mkinitcpio.conf
+		cp Backup/mkinitcpio.conf /etc/
+fi
+
+
