aedf706f34a6c0e36641c78699b081d8fbd9acfe
RJ Trujillo <certifiedblyndguy@gmail.com>
2020-06-05 20:42:10 -0600

    manjaro_help: Clean script and improve clarity
    Signed-off-by: RJ Trujillo <certifiedblyndguy@gmail.com>

--- manjaro_help.sh
+++ manjaro_help.sh
@@ -1,10 +1,10 @@
-#Based on https://wiki.archlinux.org/index.php/PCI_passthrough_via_OVMF#Using_identical_guest_and_host_GPUs
-#and
-#https://forum.manjaro.org/t/virt-manager-fails-to-detect-ovmf-uefi-firmware/110072
+# Sources:
+# https://wiki.archlinux.org/index.php/PCI_passthrough_via_OVMF#Using_identical_guest_and_host_GPUs
+# https://forum.manjaro.org/t/virt-manager-fails-to-detect-ovmf-uefi-firmware/110072
 
 VERSION=`lsb_release -r | cut -d '	' -f 2 | cut -d '.' -f 1`
 
-#Create a backup of files that will be changed
+# Backup files
 
 mkdir Backup
 chmod +x uninstall.sh
@@ -12,55 +12,43 @@ chmod +x uninstall.sh
 cp /etc/default/grub Backup
 cp /etc/mkinitcpio.conf Backup
 
-#Edit grub amd_iommu=on or intel_iommu=on rd.driver.pre=vfio-pci kvm.ignore_msrs=1
+# Edit grub amd_iommu=on or intel_iommu=on rd.driver.pre=vfio-pci kvm.ignore_msrs=1
 
 nano /etc/default/grub
-
 update-grub
 
-#Install required packages
+# Install required packages
 
 pacman -S vim qemu virt-manager ovmf dnsmasq ebtables iptables
 
-#Enable libvirt to autostart
+# Allow libvirt to autostart
 
 systemctl enable libvirtd.service
 
-#Copy necessary files
+# Copy necessary files
 
 cp vfio-pci-override.sh /usr/bin/vfio-pci-override.sh
-
 chmod +x /usr/bin/vfio-pci-override.sh
-
 cp vfio-install /etc/initcpio/install/vfio
-
 cp vfio-hooks /etc/initcpio/hooks/vfio
-
 cp vfio.conf /etc/modprobe.d/
 
-#Edit mkinitcpio.conf
+# Edit mkinitcpio.conf
 
-#MODULES="vfio_pci vfio vfio_iommu_type1 vfio_virqfd"
-#FILES="/usr/bin/vfio-pci-override.sh"
-#HOOKS="... vfio ...."
+# MODULES="vfio_pci vfio vfio_iommu_type1 vfio_virqfd"
+# FILES="/usr/bin/vfio-pci-override.sh"
+# HOOKS="... vfio ...."
 
 nano /etc/mkinitcpio.conf
-
 mkinitcpio -P
 
-#Earlier versions of Manjaro need this workaround
+# Workaround libvirt bug in previous versions of Manjaro:
+# https://forum.manjaro.org/t/virt-manager-fails-to-detect-ovmf-uefi-firmware/110072
 
 if [ $VERSION -lt 20 ]
-	then 
-
-		#Source: https://forum.manjaro.org/t/virt-manager-fails-to-detect-ovmf-uefi-firmware/110072
-
+	then
 		echo 'nvram = [	"/usr/share/ovmf/x64/OVMF_CODE.fd:/usr/share/ovmf/x64/OVMF_VARS.fd"	]' >> /etc/libvirt/qemu.conf 
-
 		mkdir -p /etc/qemu/firmware
-
 		sed 's#qemu/edk2-x86_64-code.fd#ovmf/x64/OVMF_CODE.fd#;s#qemu/edk2-i386-vars.fd#ovmf/x64/OVMF_VARS.fd#' < /usr/share/qemu/firmware/60-edk2-x86_64.json > /etc/qemu/firmware/10-ovmf-workaround.json
-
 		systemctl restart libvirtd
-
 fi
